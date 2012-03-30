f5f3c1a69fc63c9ba116821a914f057a229be8d1
Andrew Fray <andrew.fray@gmail.com>
2012-03-30 14:49:07 +0100

    added batch file for easy launching of chrome

---
+++ runinchrome.bat
@@ -0,0 +1,2 @@
+C:\Users\fraya.EU\AppData\Local\Google\Chrome\Application\chrome.exe --disable-web-security --new-window --allow-file-access-from-files file:///C:/Users/fraya.EU/Documents/gitdev/melonjstutorial/index.html
+
